ca6c5295d8daccc05339012cdf3e320cd02f2538
motleysixxx96 <danielbclarke92@gmail.com>
2018-03-07 16:08:42 -0500

    uploading lab 1 corrections

---
+++ Navbar-project lab 1/Navbar-Project/2 new folders/popup.js
@@ -0,0 +1,18 @@
+
+
+console.log("popups.js loaded");
+let answer = prompt("Restaurant bill is");
+//console.log("The user is "+answer+".")
+let tip = answer*.15;
+alert("Tonights bill is "+tip+",")
+
+/*Your first task today
+Create a program that asks for the
+cost of dinner
+
+and calculates the tip
+
+and displays the tip in an alert window
+
+a tip is at least 15% of the total check
+*/
---
+++ Navbar-project lab 1/popups2.js
@@ -0,0 +1,5 @@
+console.log("popups.js loaded");
+let gallons =prompt("How Much Gas ");
+let miles =prompt("How Many miles driven");
+let mpg= miles/gallons;
+alert("Miles per Gallon is "+mpg+",");
