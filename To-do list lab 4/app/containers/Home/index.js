c2f7ebfb9dfecb1224467737b99b6cbafd679002
motleysixxx96 <danielbclarke92@gmail.com>
2018-03-15 13:10:06 -0400

    update for lab 5

--- To-do list lab 4/app/containers/Home/index.js
+++ To-do list lab 4/app/containers/Home/index.js
@@ -11,12 +11,88 @@ import './style.css';
 import './styleM.css';
 
 export default class Home extends React.PureComponent {
+
+  constructor() {
+    super();
+    this.state = {
+      listItems:[],
+      inputItem:""
+    }
+  };
+
+componentWillMount() {
+  this.getTasks();
+}
+
+  handleItem = (event) => {
+    this.setState({
+      inputItem:event.target.value
+    })
+  };
+
+  getTasks = () => {
+    fetch('http://localhost:8000/api/getTasks', {
+      method:'GET'
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      this.setState({
+        listItems:json.tasks
+      })
+
+    }.bind(this))
+  };
+
+    storeTask = () => {
+      let data = new FormData();
+      data.append('taskContent', this.state.inputItem);
+
+        fetch('http://localhost:8000/api/storeTask', {
+          method:'POST',
+          body:data
+        })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(json) {
+          let listItems = this.state.listItems;
+          listItems.push(json.task);
+          this.setState({
+            listItems:listItems
+          })
+          this.forceUpdate();
+        }.bind(this))
+      };
+
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <div className="container">
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
-        //Remove this line and you can start writing your code here.
+
+      <div className="inputContainer">
+      <input type="text" className="todoinput"
+      onChange={this.handleItem} value={this.state.inputItem}/>
+    <input type="submit" value="Add to List" className="todoButton" onClick={this.storeTask}/>
+  </div>
+  <div className="todoList">
+    {this.state.listItems.map((item, index) => (
+      <div className="listItem" key={index} onClick={this.strikeThrough}>
+        {item.taskContent}
+        </div>
+          ))}
       </div>
-    );
+    </div>
+   );
   }
 }
