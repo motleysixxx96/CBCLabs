1a485ed4ef789c6b89327637b47c3ec96564c9b2
motleysixxx96 <danielbclarke92@gmail.com>
2018-03-16 16:19:40 -0400

    adding lab 7,8,9,and 10

---
+++ lab 9 photo upload/photo upload lab 9/app/Http/Controllers/PhotosController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Response;
+use Purifier;
+
+use App\Photo;
+
+class PhotosController extends Controller
+{
+
+  public function store(Request $request)
+  {
+  $rules = [
+    "photos" => 'required'
+  ];
+
+
+  $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+  if($validator->fails())
+  {
+    return Response::json(['error' => "Please fill out all fields."]);
+  }
+  $photoInput = $request->file('photos');
+  $photoName = $photoInput->getClientOriginalName();
+  $photoInput->move("storage/", $photoName);
+
+  $photo = new Photo;
+  $photo->photoURL = $request->root(). "/storage/".$photoName;
+  $photo->save();
+
+  return Response::json(['success' => 'Your Photo was Uploaded']);
+}
+}
---
+++ lab 9 photo upload/photo upload lab 9/foo.php
@@ -0,0 +1,9 @@
+<?php
+	$rules = [ 
+	  "photos" => "required"
+  	];
+
+	$array = [ "one","two"];
+
+	echo $rules['photos'];
+?>
